192b8896c1f5961064b130a45f22590634515de5
mariome22 <martinmarioes17@gmail.com>
2026-04-04 19:22:56 +0200

    continuacion nivel 1

--- ResetGame/Assets/Scripts/Objetos/ObjetoRompible.cs
+++ ResetGame/Assets/Scripts/Objetos/ObjetoRompible.cs
@@ -1,12 +1,17 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 
 public class ObjetoRompible : MonoBehaviour
 {
-    [Header("Configuración de la Caja")]
+    [Header("Configuracion de la Caja")]
     public int golpesParaRomper = 3;
 
+    [Header("FeedBack Visual")]
+    public float duracionShake = 0.1f;
+    public float magnitudShake = 0.1f;
+    private bool estaAgitandose = false;
+
     [Header("Recompensas (Drop Aleatorio)")]
-    [Tooltip("Arrastra aquí los PREFABS (el objeto físico del suelo, no la ficha ScriptableObject)")]
+    [Tooltip("Arrastra aqui los PREFABS (el objeto fisico del suelo, no la ficha ScriptableObject)")]
     public GameObject[] posiblesDrops;
 
     public void RecibirDano(int cantidadDano)
@@ -19,26 +24,52 @@ public class ObjetoRompible : MonoBehaviour
         {
             Romper();
         }
+        else
+        {
+            if (!estaAgitandose)
+            {
+                StartCoroutine(ShakeCoroutine());
+            }
+        }
+    }
+
+    private System.Collections.IEnumerator ShakeCoroutine()
+    {
+        estaAgitandose = true;
+        Vector3 posActual = transform.position;
+        float tiempo = 0f;
+
+        while (tiempo < duracionShake)
+        {
+            float xOffset = Random.Range(-1f, 1f) * magnitudShake;
+            float yOffset = Random.Range(-1f, 1f) * magnitudShake;
+
+            transform.position = posActual + new Vector3(xOffset, yOffset, 0f);
+
+            tiempo += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = posActual;
+        estaAgitandose = false;
     }
 
     private void Romper()
     {
         if (posiblesDrops != null && posiblesDrops.Length > 0)
         {
-            //Random.Range con números enteros incluye el primero, pero EXCLUYE el último. 
-            //Por eso ponemos (0, Length).
             int indiceAleatorio = Random.Range(0, posiblesDrops.Length);
             GameObject dropElegido = posiblesDrops[indiceAleatorio];
 
             if (dropElegido != null)
             {
                 Instantiate(dropElegido, transform.position, Quaternion.identity);
-                Debug.Log("La caja se rompió y soltó un objeto.");
+                Debug.Log("La caja se rompio y solto un objeto.");
             }
         }
         else
         {
-            Debug.Log("La caja se rompió pero estaba vacía (no pusiste drops en el Inspector).");
+            Debug.Log("La caja se rompio pero estaba vacia.");
         }
 
         Destroy(gameObject);
